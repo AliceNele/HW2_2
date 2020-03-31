d6afc907d653718c0dfdda7dd5f4e37228363988
WildfootW <wildfootw@wildfoo.tw>
2020-04-01 01:09:21 +0800

    add a function for get data path

--- HW3_2/HW3_2_functions.py
+++ HW3_2/HW3_2_functions.py
@@ -8,21 +8,30 @@ from HW3_2_class import *
 import pickle
 import os
 
+def get_data_path(category, filename = ""):
+    if filename == "":
+        return "./info/" + category
+    else:
+        return "./info/" + category + "/" + filename + ".pkl"
+
+def check_data_exist(category, filename):
+    return (filename + ".pkl") in os.listdir(get_data_path(category))
+
 def create_credit_user(): # 創信用卡帳戶
     name = input("請輸入姓名: ")
     idnumber = input("請輸入身分證字號: ")
-    if (idnumber + ".pkl") in os.listdir("./info/credit"): # 先用身分證字號檢查是否已有可用帳戶
+    if check_data_exist("credit", idnumber): # 先用身分證字號檢查是否已有可用帳戶
         print("已有可用帳戶")
     else: #沒有可用帳戶，繼續輸入信用額度
         credit = int(input("請輸入信用額度: "))
         data = CreditCardAccount(name, idnumber, credit) # 把資料存到data裡面
-        with open('./info/credit/'+ idnumber +".pkl","wb") as info:
+        with open(get_data_path("credit", idnumber),"wb") as info:
             pickle.dump(data, info)
 
 def create_bank_user(): # 創銀行帳戶
     name = input("請輸入姓名: ")
     idnumber = input("請輸入身分證字號: ")
-    if (idnumber + ".pkl") in os.listdir("./info/bank"):#先用身分證字號檢查是否已有可用帳戶
+    if check_data_exist("bank", idnumber):#先用身分證字號檢查是否已有可用帳戶
         print("已有可用帳戶")
     else: #沒有可用帳戶，繼續輸入密碼
         password = input("請輸入密碼: ")
@@ -31,7 +40,7 @@ def create_bank_user(): # 創銀行帳戶
             password = input("密碼不符合規定，請重新輸入: ")
             check = check_password(password)
         data = BankAccount(name, idnumber, password)
-        with open('./info/bank/'+idnumber+'.pkl','wb') as info:
+        with open(get_data_path("bank", idnumber),'wb') as info:
             pickle.dump(data, info)
 
 def check_password(password): # 檢查密碼
@@ -65,17 +74,15 @@ def check_password(password): # 檢查密碼
 def pay_creditcard_fee():
     idnumber = input("請輸入身分證字號: ")
     #檢查是否有銀行跟信用卡帳戶
-    if ((idnumber + ".pkl") in os.listdir("./info/bank/")) and ((idnumber + ".pkl") in os.listdir("./info/credit/")): 
+    if check_data_exist("bank", idnumber) and check_data_exist("credit", idnumber):
         password = input("儲蓄存款帳戶密碼: ")
-        with open('./info/bank/'+idnumber + '.pkl','rb') as info: #開啟資料
+        with open(get_data_path("bank", idnumber),'rb') as info: #開啟資料
             user = pickle.load(info)
         if password != user.password:#檢查密碼是否正確，情況3
             print("密碼錯誤")
         else: #密碼對了
             print("密碼答對")
-            with open('./info/bank/'+idnumber + '.pkl','rb') as info: #開啟銀行資料
-                user = pickle.load(info)
-            with open('./info/credit/'+idnumber + '.pkl','rb') as info: #開啟信用卡資料
+            with open(get_data_path("credit", idnumber),'rb') as info: #開啟信用卡資料
                 account = pickle.load(info)
             if user.inmoney >= account.used:  #儲蓄帳戶餘額 > 信用卡費，情況4
                 print("繳費前餘額: %d" % user.inmoney)
@@ -89,9 +96,9 @@ def pay_creditcard_fee():
             else: #儲蓄帳戶餘額不足，情況5
                 print( "餘額不足")
     #有銀行但沒有信用卡帳戶，情況2
-    elif ((idnumber + ".pkl") in os.listdir("./info/bank/")) and ((idnumber + ".pkl") not in os.listdir("./info/credit/")): 
+    elif check_data_exist("bank", idnumber) and not check_data_exist("credit", idnumber):
         print( "無須繳費")
     #沒有銀行但有信用卡帳戶，情況1
-    elif ((idnumber + ".pkl") not in os.listdir("./info/bank/")) and ((idnumber + ".pkl") in os.listdir("./info/credit/")):
+    elif not check_data_exist("bank", idnumber) and check_data_exist("credit", idnumber):
         print("請先開立帳戶")
         create_bank_user()
